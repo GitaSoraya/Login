d60a3c1b900536b76891f2f3b1ea042c04fde5db
GitaSoraya <127930678+GitaSoraya@users.noreply.github.com>
2023-06-28 14:03:46 +0700

    login

---
+++ app/src/main/java/com/gitasoraya201103651/login/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.gitasoraya201103651.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
